a67984c76a176d50dfd8c2d9643f6d69bb7575df
Piyush <piyushshrivastava077@gmail.com>
2017-11-05 18:44:15 +0530

    Added comments

--- include/config.php
+++ include/config.php
@@ -1,7 +1,7 @@
 <?php
 
 //Database config variables
-define("DB_HOST", "localhost");
+define("DB_HOST", "localhost"); //Enter your server IP here
 define("DB_USER", "root");
 define("DB_PASSWORD", "root");
 define("DB_DATABASE", "bl_users");
