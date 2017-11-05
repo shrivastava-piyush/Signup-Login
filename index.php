1c743a3bf3f3e95c15c4077c0c195dd1123fa0f3
Piyush <piyushshrivastava077@gmail.com>
2017-11-05 18:14:38 +0530

    Updated index page

---
+++ index.php
@@ -0,0 +1,9 @@
+<h3>APIs for Signup and Login</h3><br/>
+
+For Login:<br/>
+https://blogappsignin.herokuapp.com/login.php<br/><br/>
+
+For Registration:<br/>
+https://blogappsignin.herokuapp.com/register.php<br/><br/>
+
+Created by - Piyush Shrivastava
